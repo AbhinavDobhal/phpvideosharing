34abfeeb0fa3fd77b2521c9956069bc821c8fa84
Alvin Abhinav <alvin.abhinav@ithands.net>
2019-12-15 20:53:40 +0530

    first commit

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.playtubescript.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com   
+// +------------------------------------------------------------------------+
+// | PlayTube - The Ultimate Video Sharing Platform
+// | Copyright (c) 2017 PlayTube. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "127.0.0.1";
+// MySQL Database User
+$sql_db_user = "root";
+// MySQL Database Password
+$sql_db_pass = "aarav#21";
+// MySQL Database Name
+$sql_db_name = "suarakami";
+
+// Site URL
+$site_url = "https://pvs.app"; // e.g (http://example.com)
+
+// Purchase code
+$purchase_code = "e02018e3-40e3-4ef6-8db8-30c7d1a19e2f"; // Your purchase code, don't give it to anyone. 
+?>
